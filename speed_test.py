e91fc5f4f46e8a1d73190f6a98ea60aab7a6e556
rxxzeee <salatenko7@gmail.com>
2024-07-19 00:28:26 +0300

    Add code on GitHub

---
+++ speed_test.py
@@ -0,0 +1,27 @@
+﻿import speedtest
+import time
+
+def test_internet_speed():
+    
+    st = speedtest.Speedtest()
+    
+    print("Поиск лучших серверов...")
+    st.get_best_server
+    
+    print("Измерение скорости загрузки...")
+    download_speed = st.download()
+    
+    print("Измерение скорости выгрузки...")
+    upload_speed = st.upload()
+    
+    print("Измерение пинга...")
+    ping = st.results.ping
+    
+    print(f"Скорость загрузки: {download_speed / 1_000_000:.2f} Мбит/с")
+    print(f"Скорость выгрузки: {upload_speed / 1_000_000:.2f} Мбит/с")
+    print(f"Задержка: {ping} мс")
+
+if __name__ == "__main__":
+    test_internet_speed()
+    while(True):
+        time.sleep(1)
